875f421d13446ff04a246d4903ff9f775bc287d2
Ram-Repo1984 <96728352+Ram-Repo1984@users.noreply.github.com>
2022-02-06 16:16:49 -0300

    producto.html
    upload producto.html

---
+++ js/lightbox.js
@@ -0,0 +1,31 @@
+// Generacion de la seleccion de imagenes y el zoom, intercambio de imagenes en la class"agregar-imagen" que se encuentra vacia
+// 
+
+
+
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+const hamburguer1 = document.querySelector('.hamburger')
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+        
+    })
+})
+
+contenedorLight.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImagen')
+        hamburguer1.style.opacity = "1"
+    }
+})
+
+const aparecerImagen = (imagen)=>{
+   imagenesLight.src = imagen
+   contenedorLight.classList.toggle('show')
+   imagenesLight.classList.toggle('showImagen')
+   hamburguer1.style.opacity = "0"
+};
